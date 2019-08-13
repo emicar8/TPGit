4c1c58ac5d2c61f8a178876188cf93733e359c39
PaulaColque <paulajulieta@live.com.ar>
2019-08-13 12:14:53 -0300

    Agrego clase de prueba para practicar proyectos colaborativos

---
+++ src/tpgit/Paula.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tpgit;
+
+/**
+ *
+ * @author Colque
+ */
+public class Paula {
+    void presentacion(){
+        System.out.println("Hola, soy Paula");
+    }
+}
